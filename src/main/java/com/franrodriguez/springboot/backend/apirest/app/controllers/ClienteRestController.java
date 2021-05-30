da9fdd420f71737e9d2fb62f19a0df88dd693c46
Francisco <fran89rbb@hotmail.com>
2021-05-30 16:56:05 +0200

    Añadiendo demás métodos crud

--- src/main/java/com/franrodriguez/springboot/backend/apirest/app/controllers/ClienteRestController.java
+++ src/main/java/com/franrodriguez/springboot/backend/apirest/app/controllers/ClienteRestController.java
@@ -3,9 +3,16 @@ package com.franrodriguez.springboot.backend.apirest.app.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.franrodriguez.springboot.backend.apirest.app.models.entity.Cliente;
@@ -22,6 +29,36 @@ public class ClienteRestController {
 	@GetMapping("/clientes")
 	public List<Cliente> index(){
 		return clienteService.findAll();
+	}
+	
+	@GetMapping("/clientes/{id}")
+	@ResponseStatus(value = HttpStatus.OK)
+	public Cliente show(@PathVariable Long id) {
+		return clienteService.findById(id);
+	}
+	
+	@PostMapping("/clientes")
+	@ResponseStatus(value = HttpStatus.CREATED)
+	public Cliente create(@RequestBody Cliente cliente) {
+		return clienteService.save(cliente);
+	}
+	
+	@PutMapping("/clientes/{id}")
+	@ResponseStatus(value = HttpStatus.CREATED)
+	public Cliente update(@RequestBody Cliente cliente, @PathVariable Long id) {
+		Cliente clienteActual = clienteService.findById(id);
+		
+		clienteActual.setNombre(cliente.getNombre());
+		clienteActual.setApellido(cliente.getApellido());
+		clienteActual.setEmail(cliente.getEmail());
+		
+		return clienteService.save(clienteActual);
+	}
+	
+	@DeleteMapping("/clientes/{id}")
+	@ResponseStatus(value = HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long id) {
+		clienteService.delete(id);
 	}
 
 }
--- src/main/java/com/franrodriguez/springboot/backend/apirest/app/models/entity/Cliente.java
+++ src/main/java/com/franrodriguez/springboot/backend/apirest/app/models/entity/Cliente.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -66,6 +67,11 @@ public class Cliente implements Serializable{
 
 	public void setCreateAt(Date createAt) {
 		this.createAt = createAt;
+	}
+	
+	@PrePersist
+	public void prePersist() {
+		createAt = new Date();
 	}
 	
 	/**
